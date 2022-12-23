f6dd42be34ed7883348767372ee2e1eee1ec61ef
Olayinka Akeju <akejuolayinka016@gmail.com>
2022-12-23 11:00:00 +0100

    Created components for activity, card and card-row

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivityComponent } from './components/activity/activity.component';
+import { CardComponent } from './components/card/card.component';
+import { CardRowComponent } from './components/card-row/card-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityComponent,
+    CardComponent,
+    CardRowComponent
   ],
   imports: [
     BrowserModule,
