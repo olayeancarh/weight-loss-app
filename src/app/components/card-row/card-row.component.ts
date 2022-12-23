cd31fdb64175fd20b590a2eee41a73973d32bc80
Olayinka Akeju <akejuolayinka016@gmail.com>
2022-12-23 16:26:55 +0100

    Card row section completed

--- src/app/components/card-row/card-row.component.ts
+++ src/app/components/card-row/card-row.component.ts
@@ -12,4 +12,12 @@ export class CardRowComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  cardContents(): any {
+    return [
+      {val: 866, text: 'Calories burn', svg: 'assets/images/fire.svg', percentage: '60%'},
+      {val: 7996, text: 'Steps', svg: 'assets/images/feet.svg', percentage: '45%'},
+      {val: 21, text: 'Weight loss', svg: 'assets/images/fmeter.svg', percentage: '80%'},
+    ]
+  }
+
 }
