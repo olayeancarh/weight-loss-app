7eae18c2f712f9eb73d42e2793d717668699b1cf
Olayinka Akeju <akejuolayinka016@gmail.com>
2022-12-28 00:03:48 +0100

    Created second page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weight-loss-app';
+
+  ngOnInit(): void {
+    let clickEvent = document.getElementById('pageClick')?.addEventListener('click', (e) => this.getClickPosition(e))
+  }
+
+  getClickPosition(e: MouseEvent): void {
+    let xPosition = e.clientX;
+    let yPosition = e.clientY;
+
+    // console.log(e.target)
+  }
 }
